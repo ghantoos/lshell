816a3b97aeceda901271e3d39d167d753fcdb683
Ignace Mouzannar <mouzannar@gmail.com>
2024-10-28 23:19:59 -0400

    Update lexer/parser - Initial update for #87
    Closes: #118

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,8 @@
 """ This module is used to run all the tests in the test directory. """
 
-from test import test_functional, test_unit
+from test import test_functional_p1, test_unit, test_functional_p2
 
 if __name__ == "__main__":
-    test_functional.unittest.main()
+    test_functional_p1.unittest.main()
+    test_functional_p2.unittest.main()
     test_unit.unittest.main()

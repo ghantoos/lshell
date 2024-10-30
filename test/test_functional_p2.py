7b2d78ebd2843103a10dd9bc933863c2ff5a49ce
Ignace Mouzannar <ghantoos@users.noreply.github.com>
2024-10-29 22:23:15 -0400

    Fix Bug: allow all but some commands (Closes #213) (#238)
    * Fix def myeval() for allowed diff in config (Closes: #213)
    * Add associated functional tests

--- test/test_functional_p2.py
+++ test/test_functional_p2.py
@@ -27,7 +27,7 @@ class TestFunctions(unittest.TestCase):
         self.child.close()
 
     def test_51_grep_valid_log_entry(self):
-        """Test that grep matches a valid log entry format."""
+        """F51 | Test that grep matches a valid log entry format."""
         pattern = (
             r"\[\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s+(?:-|\+)\d{4}\].+UID=[\w.]+"
         )
@@ -49,7 +49,7 @@ class TestFunctions(unittest.TestCase):
         self.assertIn("user123", output)
 
     def test_52_grep_invalid_date_format(self):
-        """Test that grep matches a valid log entry format."""
+        """F52 | Test that grep matches a valid log entry format."""
         pattern = (
             r"\[\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s+(?:-|\+)\d{4}\].+UID=[\w.]+"
         )
@@ -69,7 +69,7 @@ class TestFunctions(unittest.TestCase):
         self.assertNotIn("user123", output)
 
     def test_53_grep_missing_uid(self):
-        """Test that grep matches a valid log entry format."""
+        """F53 | Test that grep matches a valid log entry format."""
         pattern = (
             r"\[\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s+(?:-|\+)\d{4}\].+UID=[\w.]+"
         )
@@ -89,7 +89,7 @@ class TestFunctions(unittest.TestCase):
         self.assertNotIn("user123", output)
 
     def test_54_grep_special_characters_in_uid(self):
-        """Test that grep matches a valid log entry format."""
+        """F54 | Test that grep matches a valid log entry format."""
         pattern = (
             r"\[\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s+(?:-|\+)\d{4}\].+UID=[\w.]+"
         )
@@ -107,3 +107,131 @@ class TestFunctions(unittest.TestCase):
         self.child.expect(f"{self.user}:~\\$")
         output = self.child.before.decode("utf-8")
         self.assertIn("user.name", output)
+
+    def test_55_allowed_all_minus_list(self):
+        """F55 | allow all commands minus the list"""
+
+        command = "echo 1"
+        expected = "*** forbidden command: echo"
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} --config {TOPDIR}/etc/lshell.conf "
+            '--allowed \'"all" - ["echo"]'
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command)
+        self.child.expect(f"{self.user}:~\\$")
+        output = self.child.before.decode("utf-8").split("\n")[1].strip()
+        self.assertEqual(expected, output)
+
+    def test_56_path_minus_specific_path(self):
+        """F56 | allow paths except for the specified path"""
+
+        command1 = "cd /usr/"
+        expected1 = f"{self.user}:/usr\\$"
+        command2 = "cd /usr/local"
+        expected2 = "*** forbidden path: /usr/local/"
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} --config {TOPDIR}/etc/lshell.conf "
+            '--path \'["/var", "/usr"] - ["/usr/local"]\''
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command1)
+        self.child.expect(expected1)
+        self.child.sendline(command2)
+        self.child.expect(expected1)
+        output = self.child.before.decode("utf-8").split("\n")[1].strip()
+        self.assertEqual(expected2, output)
+
+    def test_57_overssh_all_minus_list(self):
+        """F57 | overssh minus command list"""
+        command = "echo 1"
+        expected = (
+            '*** forbidden char/command over SSH: "echo 1"\r\n'
+            "This incident has been reported."
+        )
+
+        # add SSH_CLIENT to environment
+        if not os.environ.get("SSH_CLIENT"):
+            os.environ["SSH_CLIENT"] = "random"
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} "
+            f"--config {CONFIG} "
+            f"--overssh \"['ls','echo'] - ['echo']\" "
+            f"-c '{command}'"
+        )
+        self.child.expect(pexpect.EOF)
+
+        output = self.child.before.decode("utf-8").strip()
+        self.assertEqual(expected, output)
+
+    def test_58_allowed_plus_minus_list(self):
+        """F58 | allow plus list minus list"""
+        command = "echo 1"
+        expected = "*** forbidden command: echo"
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} "
+            f"--config {CONFIG} "
+            f"--allowed \"['ls'] + ['echo'] - ['echo']\" "
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command)
+        self.child.expect(f"{self.user}:~\\$")
+        output = self.child.before.decode("utf-8").split("\n")[1].strip()
+        self.assertEqual(expected, output)
+
+    def test_59a_forbidden_remove_one(self):
+        """U59a | remove all items from forbidden list"""
+
+        command = "echo 1 ; echo 2"
+        expected = [" echo 1 ; echo 2\r", "1\r", "2\r", ""]
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} --config {TOPDIR}/etc/lshell.conf "
+            '--forbidden \'[";"] - [";"]\''
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command)
+        self.child.expect(f"{self.user}:~\\$")
+        output = self.child.before.decode("utf-8").split("\n")
+        self.assertEqual(expected, output)
+
+    def test_59b_forbidden_remove_one(self):
+        """U59b | fixed forbidden list"""
+
+        command = "echo 1 ; echo 2"
+        expected = "*** forbidden character: ;"
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} --config {TOPDIR}/etc/lshell.conf " "--forbidden '[\";\"]'"
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command)
+        self.child.expect(f"{self.user}:~\\$")
+        output = self.child.before.decode("utf-8").split("\n")[1].strip()
+        self.assertEqual(expected, output)
+
+    def test_59c_forbidden_remove_one(self):
+        """U59c | remove an item from forbidden list"""
+
+        command = "echo 1 ; echo 2"
+        expected = [" echo 1 ; echo 2\r", "1\r", "2\r", ""]
+
+        self.child = pexpect.spawn(
+            f"{LSHELL} --config {TOPDIR}/etc/lshell.conf "
+            '--forbidden \'[";", "|", "%"] - [";"]\''
+        )
+        self.child.expect(f"{self.user}:~\\$")
+
+        self.child.sendline(command)
+        self.child.expect(f"{self.user}:~\\$")
+        output = self.child.before.decode("utf-8").split("\n")
+        self.assertEqual(expected, output)

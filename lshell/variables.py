a4fda5987be39c198f4bb49990ffd7e9efa0a9be
Ignace Mouzannar <ignace@haloo.ai>
2026-02-28 22:45:54 -0500

    Bump version to 1.0.0rc2

--- lshell/variables.py
+++ lshell/variables.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0rc2"
 
 # Required config variable list per user
 required_config = ["allowed", "forbidden", "warning_counter"]
